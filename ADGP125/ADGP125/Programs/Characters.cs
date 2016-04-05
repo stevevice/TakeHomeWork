c23b3744f36b8051c76c6c703a0e6f8e1ac0984c
stevevice <steve_alexanders@bellsouth.net>
2016-04-05 18:06:39 -0500

    character code

---
+++ ADGP125/ADGP125/Programs/Characters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Character
+{
+    float health;
+    float attack;
+    float defense;
+    float speed;
+    float experience;
+
+    public Character(float hp, float atk, float def, float spd, float xp)
+    {
+        health = hp;
+        attack = atk;
+        defense = def;
+        speed = spd;
+        experience = xp;
+    }
+}
+
+public class Player : public Character
+{
+    float 
+
+    Player() : base ()
+    {
+
+    }
+}
