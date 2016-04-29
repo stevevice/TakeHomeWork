6296ed0f50992cd7c9a6a0f61f498c06434df7a9
stevevice <steve_alexanders@bellsouth.net>
2016-04-29 16:00:19 -0500

    Working on Serialization and Deserialization

--- ADGP125/ADGP125/Programs/Program.cs
+++ ADGP125/ADGP125/Programs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace ADGP125
 {
@@ -12,10 +13,12 @@ namespace ADGP125
         /// The main entry point for the application.
         /// </summary>
         
-
         [STAThread]
         static void Main()
         {
+
+            Character Player = new Character("Steve", 10, 2, 1, 2, 0);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ ListEnum-FSM/ListEnum-FSM/Programs/SaveLoad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+public class SaveLoad<T>
+{
+    public SaveLoad() { }
+
+    public void Serialize(string FileName, T Data)
+    {
+        using (FileStream fs = File.Create(@"..\..\SavedFiles\" + FileName + ".bin"));
+        {
+            XmlSerializer serializer = new XmlSerializer(GetType(T));
+
+
+        }
+    }
+}
